aa8b9b91c844aae2a150ecf3680edb2dab4bd721
satex <satexpconline@gmail.com>
2025-11-30 00:32:32 +0300

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,24 @@ import express from "express";
 import dotenv from "dotenv";
 import autRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", autRoutes)
 app.use("/api/messages", messageRoutes)
 
+if (process.env.NODE_ENV == "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+}
 
+app.get("*",(_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+})
 
 
 
